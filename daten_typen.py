e35659c2f3d350c615310a9d4830bbc746bf6bc2
albecabrera <albe.cabrera@yahoo.de>
2025-12-22 19:23:45 +0100

    len

---
+++ daten_typen.py
@@ -0,0 +1,15 @@
+# Datentypen
+x = "Hallo Leandro, wie geht es dir heute denn?"
+print(x)
+
+# indexing
+y = "Alberto"
+print(y[2])
+print(y[2-5])
+print(y[2:7])
+print(y[:5])
+
+satz_laenge = "Wie lange ist dieser Satz?"
+print(len(satz_laenge)) # bestimmt die Länge eines Satzes
+print("Der String mit der Variable Satzlänge ist", len(satz_laenge), "Zeichen lang")
+
---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hola mundo") # mit einfachen Anführungszeichen '' oder mit doppeltem Anführungszeichen
+print('Hola mundo')
+print('Ich habe Hallo Welt programmiert und bin bald bald ein Python Profi')
+
+# VARIABLEN
+x = 5
+print(x)
+# Was würde dann passieren, wenn ich print("x") ausgeben?
+print("x") # Antwort: x
+
+firmenname = "CabreDevSuperFirma"
+weiterbildungsdauer = 6
+print("Hallo liebes Team von " + firmenname)
+print("Der Name meiner Firma ist: " + firmenname)
+print("Die Dauer meine Weiterbildungsdauer ist: " + str(weiterbildungsdauer) + " Monate")
+
+
+
+
+
